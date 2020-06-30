9992fb3514ffcf3d715cd6584c2741d45d8e6f7e
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-06-30 15:08:18 -0400

    Testing flask

--- wsgi.py
+++ wsgi.py
@@ -3,5 +3,9 @@ from application import create_app
 
 app = create_app()
 
+@app.route('/')
+def index():
+    return "<h1>Welcome to our Flask Backend ... </h1>
+
 if __name__ == "__main__":
     app.run()
