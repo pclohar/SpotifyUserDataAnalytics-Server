8d70098167313a0ab22df1c897ba23399721c081
Pallavi Lohar <pallavi.lohar18@gmail.com>
2020-07-09 13:42:44 -0400

    added redis

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+import redis
 
 def create_app():
     # create and configure the app
     flaskApp = Flask(__name__, instance_relative_config=True)
     CORS(flaskApp)
     flaskApp.config.from_mapping(
-        SECRET_KEY = b';y\xd3\xd3\xe5\xa6\x119(&;Ea\x17\xfe\xdc'
+        SECRET_KEY = b';y\xd3\xd3\xe5\xa6\x119(&;Ea\x17\xfe\xdc',
+        SESSION_TYPE = os.environ.get('redis'),
+        SESSION_REDIS = redis.from_url('redis://redistogo:21cf5b4b4a9adbe470dfb5390040502c@pike.redistogo.com:11760/')
     )
 
     
